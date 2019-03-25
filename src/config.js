0aa39426ab630e3d38484545f8665b7c6c1745ce
Renato Castilho de Matos <rcastilho@id.uff.br>
2019-03-25 10:31:53 -0300

    icones e navbar

---
+++ src/config.js
@@ -0,0 +1,17 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "minions-pedidos"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://959nxj3ex7.execute-api.us-east-1.amazonaws.com/dev"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_2PXtyo81e",
+      APP_CLIENT_ID: "5vek9bqmj9ugdu07urjhju8hfb",
+      IDENTITY_POOL_ID: "us-east-1:e5baa553-22e4-4e62-8ae4-537c2a3c11be"
+    }
+  };
+  
---
+++ src/containers/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>Minion Store</h1>
+          <p>Minions ipsum wiiiii irure aliqua. Pepete esse dolore para tú baboiii.</p>
+        </div>
+      </div>
+    );
+  }
+}
